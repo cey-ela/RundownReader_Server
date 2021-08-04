dfba6b7ac05a10308cb742c7a67fcfe7e571fc0a
joe <joeedwards88@gmail.com>
2021-08-04 12:34:33 +0100

    test

--- retrieve_rundown_gb.py
+++ retrieve_rundown_gb.py
@@ -11,12 +11,14 @@ from itertools import cycle
 from kivy import properties as kp
 import time
 from func_timeout import func_timeout, FunctionTimedOut
+from email_notification import email_error_notification as een
 
 
 class InewsPullSortSaveGB:
     app = None
     console = None
     epoch_time = int(time.time())
+    timeout_counter = 0
 
     def __init__(self):
         self.story_ids = []  # list of story_id titles in string form. E.g. 'E45RF34'
@@ -36,18 +38,19 @@ class InewsPullSortSaveGB:
 
         except FunctionTimedOut:
             self.app.console_log(filename, color + '...RETR cmd hung, retrying...[/color]')
-            print('RETR cmd hung, retrying')
+            print('RETR cmd hung, retrying ' + str(datetime.datetime.now()))
             return self.init_process(inews_path, local_dir, filename, color)
 
         except TimeoutError:
             self.app.console_log(filename, color + '...connection to iNews timeout, check connection...[/color]')
-            print('General IP connection failure')
+            print('General IP connection failure ' + str(datetime.datetime.now()))
             return self.init_process(inews_path, local_dir, filename, color)
 
         except all_errors:
-            print('FTP - general error\n' * 10)
+            print('FTP - general error ' + str(datetime.datetime.now()))
             self.app.console_log(filename, color + 'FTP error..trying again...[/color]')
             return self.init_process(inews_path, local_dir, filename, color)
+
 
         # 2ND
         self.convert_xml_to_dict(local_dir)
@@ -73,7 +76,7 @@ class InewsPullSortSaveGB:
 
     def pull_xml_via_ftp(self, inews_path, local_dir, filename, color):
         counter = 0
-        with open("/Users/joseedwa/PycharmProjects/xyz/aws_creds.json") as aws_creds:
+        with open("C:\\Program Files\\RundownReader_Server\\xyz\\aws_creds.json") as aws_creds:
             inews_details = json.load(aws_creds)
             user = inews_details[1]['user']
             passwd = inews_details[1]['passwd']
@@ -112,11 +115,8 @@ class InewsPullSortSaveGB:
         # TODO: NOTE - focus, brk, and floated default false values removed, try and work App without these
         """ A bulky method that in summary, takes each raw FTP NSML story file and converts it to a dict.
         Stripping out the bits we don't need and reformatting some dict key/values along the way.
-
         By the end we are left with a neat list of dicts (self.data)
-
         NSML (XML) Example:
-
         <nsml version="-//AVID//DTD NSML 1.0//EN">
         <head>
         3<meta rate=180 float>
@@ -149,7 +149,6 @@ class InewsPullSortSaveGB:
         <f id=gfxprep></f>
         <f id=vartm-01></f>
         </fields>
-
         We only want some meta data from line 3, story_id form line 7 and then everything between /fields.
         """
 
