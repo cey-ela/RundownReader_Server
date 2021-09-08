297b056275613334940a681b278832e9e957c9eb
cey-ela <joeedwards88@gmail.com>
2021-09-08 13:01:52 +0100

    work u bastad

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from datetime import datetime
 from calendar import timegm
 import time
 import json
+from kivy import properties as kp
 
 class ConsoleApp(MDApp):
     ready = False
@@ -25,6 +26,7 @@ class ConsoleApp(MDApp):
     lw_log = ['']
     cs_log = ['']
     console = None
+    schedule = kp.ObjectProperty()
 
     automation_switches = {'gb': False,
                            'lk': False,
@@ -32,65 +34,31 @@ class ConsoleApp(MDApp):
                            'lw': False,
                            'cs': False}
 
-    # automation = {'gb': [{}, {}, {}, {}],
-    #               'lk': [{}, {}, {}, {}],
-    #               'tm': [{}, {}, {}, {}],
-    #               'lw': [{}, {}, {}, {}],
-    #               'cs': [{}, {}, {}, {}]}
-
-    with open('schedule.json') as sched:
-        automation = json.load(sched)
-
-    # automation = {'gb': [{}, {}, {}, {}],
-    #               'lk': [{'start': '07:00', 'finish': '08:45', 'freq': '300'},
-    #                      {'start': '08:45', 'finish': '09:55', 'freq': '30'}, {}, {}],
-    #               'tm': [{}, {}, {}, {}],
-    #               'lw': [{'start': '07:00', 'finish': '12:15', 'freq': '300'},
-    #                      {'start': '12:15', 'finish': '13:25', 'freq': '30'}, {}, {}],
-    #               'cs': [{}, {}, {}, {}]}
-
-    # {'gb': [{}, {}, {}, {}],
-    #  'lk': [{'start': '07:00', 'finish': '08:45', 'freq': '300'},
-    #         {{'start': '08:45', 'finish': '09:55', 'freq': '30'}}, {}, {}],
-    #  'tm': [{}, {}, {}, {}],
-    #  'lw': [{'start': '07:00', 'finish': '12:15', 'freq': '300'},
-    #         {'start': '12:15', 'finish': '13:25', 'freq': '30'}, {}, {}],
-    #  'cs': [{}, {}, {}, {}]}
 
     rundown_switch_dict = {}
 
     def build(self):
         InewsPullSortSaveGB.app = InewsPullSortSaveLK.app = InewsPullSortSaveTM.app = InewsPullSortSaveLW.app = \
             InewsPullSortSaveCS.app = self
-        # self.populate_schedule()
+        self.populate_schedule()
 
-    # def populate_schedule(self):
-    #     with open('schedule.json') as sched:
-    #         schedule = json.load(sched)
-    #
-    #     for prod in self.root.ids:
-    #         # prod[-2:] = production. eg LK, TM
-    #         if 'auto' in prod:
-    #             for input_box in self.root.ids[prod].ids:
-    #                 print(input_box)
-    #                 # self.root.ids[prod].ids[input_box].text = schedule[prod[-2:]][0]
+    def populate_schedule(self):
+        with open('schedule.json') as sched:
+            self.schedule = json.load(sched)
 
+        for prod in self.root.ids:
+            if 'auto' in prod:
+                for input_box in self.root.ids[prod].ids:
+                    self.root.ids[prod].ids[input_box].text = self.schedule[prod[-2:]][input_box]
 
+    def update_schedule(self):
+        for prod in self.root.ids:
+            if 'auto' in prod:
+                for input_box in self.root.ids[prod].ids:
+                    self.schedule[prod[-2:]][input_box] = self.root.ids[prod].ids[input_box].text
 
-        # self.root.ids.auto_lk.ids.start_one.text = '07:00'
-        # self.root.ids.auto_lk.ids.finish_one.text = '08:45'
-        # self.root.ids.auto_lk.ids.freq_one.text = '300'
-        # self.root.ids.auto_lk.ids.start_two.text = '08:45'
-        # self.root.ids.auto_lk.ids.finish_two.text = '09:55'
-        # self.root.ids.auto_lk.ids.freq_two.text = '30'
-        #
-        # self.root.ids.auto_lw.ids.start_one.text = '07:00'
-        # self.root.ids.auto_lw.ids.finish_one.text = '12:15'
-        # self.root.ids.auto_lw.ids.freq_one.text = '300'
-        # self.root.ids.auto_lw.ids.start_two.text = '12:15'
-        # self.root.ids.auto_lw.ids.finish_two.text = '13:25'
-        # self.root.ids.auto_lw.ids.freq_two.text = '30'
-
+        with open('schedule.json', 'w') as f:
+            f.write(json.dumps(self.schedule, indent=4))
 
     def rundown_switch(self, switch, rundown, local_dir, export_path, color):
         self.rundown_switch_dict[export_path] = switch
@@ -115,25 +83,54 @@ class ConsoleApp(MDApp):
     def determine_frequency(self, production):
         now = datetime.now()
         seconds_midnight = int((now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds())
+        start_h = 0
+        start_m = 0
+        fin_h = 0
+        fin_m = 0
+        freq = 0
 
         if self.automation_switches[production]:
-            print(self.automation)
 
-            for d in self.automation[production]:
-                if 'start' in d and 'finish' in d:
-                    strt_h, strt_m = d['start'].split(':')
-                    fin_h, fin_m = d['finish'].split(':')
-
-                    if ((int(strt_h) * 3600) + (int(strt_m) * 60)) < seconds_midnight\
-                            < ((int(fin_h) * 3600) + (int(fin_m) * 60)):
-                        return int(d['freq'])
-                    else:
-                        print('ZZ')
+            for x, y in self.schedule[production].items():  # Loop dict for each key(x), val(y)
+                if 'start' in x:
+                    start_h, start_m = (int(x) for x in y.split(':'))   # Store start hour and min
+                elif 'fin' in x:
+                    fin_h, fin_m = (int(x) for x in y.split(':'))   # Store finish hour and min
                 else:
-                    print('XX')
-                    return 3600
+                    freq = int(y)  # Lastly store it's freq, then before moving onto the next start item check if it's falls
+                    # within the current time frame:
 
-            return 3600
+                    if ((start_h * 3600) + (start_m * 60)) < seconds_midnight < ((fin_h * 3600) + (fin_m * 60)):
+                        return freq
+
+
+
+
+                    # start_h, start_m = d.split(':')
+
+
+                # elif 'finish' in d:
+                #     fin_h, fin_m = d.split(':')
+                # elif 'freq' in d:
+                #     freq = d
+
+            # if ((int(start_h) * 3600) + (int(start_m) * 60)) < seconds_midnight\
+            #         < ((int(fin_h) * 3600) + (int(fin_m) * 60)):
+
+
+            #         strt_h, strt_m = d['start'].split(':')
+            #         fin_h, fin_m = d['finish'].split(':')
+            #
+            #         if ((int(strt_h) * 3600) + (int(strt_m) * 60)) < seconds_midnight\
+            #                 < ((int(fin_h) * 3600) + (int(fin_m) * 60)):
+            #             return int(d['freq'])
+            #         else:
+            #             print('ZZ')
+            #     else:
+            #         print('XX')
+            #         return 3600
+            #
+            return 3650
 
         else:
             return int(eval('self.root.ids.repeat_in_seconds_' + production).text)
